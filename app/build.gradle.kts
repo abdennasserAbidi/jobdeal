425bc6ad7f9604ec1a054daaf3bfeb784c5c2d51
abdennasserAbidi <abidi.abdennasser@gmail.com>
2025-12-16 14:04:14 +0000

    chore: bump android version to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.example.myjob"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
